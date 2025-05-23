18b2d9bef4ce11f6006a3b6b4e75cf82496ab664
Noha Ahmed 01 <nohaniho88@gmail.com>
2025-05-23 19:07:48 +0300

    ll

--- assets/custom-landing.js
+++ assets/custom-landing.js
@@ -2,14 +2,12 @@ document.addEventListener("DOMContentLoaded", async () => {
   const container = document.getElementsByClassName("row")[0];
   const response = await fetch("/products.json");
   const data = await response.json();
+let images = document.getElementsByid("img1");
+  const html = data.products.map(product => `
+    <div class="col">
+      <img src="${product.images[0].src}" alt="${product.title}" />
+    </div>
+  `).join("");
 
-//   const html = data.products.map(product => `
-//     <div class="product-card">
-//       <img src="${product.images[0].src}" alt="${product.title}" />
-//       <h3>${product.title}</h3>
-//       <p>${product.variants[0].price} USD</p>
-//     </div>
-//   `).join("");
-
-//   container.innerHTML = html;
-// });
+  container.innerHTML = html;
+});
