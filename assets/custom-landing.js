90c19c72cbe0c52b17946f68e2510bb182177705
Noha Ahmed 01 <nohaniho88@gmail.com>
2025-05-23 23:22:38 +0300

    gridddd

---
+++ assets/custom-landing.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", async () => {
+  const container = document.getElementsByClassName("row")[0];
+  const response = await fetch("/products.json");
+  const data = await response.json();
+
+  const content = data.products
+    .slice(0, 6)
+    .map(
+      (product) =>
+        `<div class="col">
+        <img src = "${product.images[0].src}" alt="${product.title}"/>
+        </div>`
+    )
+    .join("");
+    container.innerHTML = content;
+});
