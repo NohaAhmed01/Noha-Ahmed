3492af6ff790c57e8dc5247b167723b1169a6552
Noha Ahmed 01 <nohaniho88@gmail.com>
2025-05-23 19:09:25 +0300

    mm

--- assets/custom-landing.js
+++ assets/custom-landing.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", async () => {
   const container = document.getElementsByClassName("row")[0];
   const response = await fetch("/products.json");
   const data = await response.json();
-let images = document.getElementsByid("img1");
+
   const html = data.products.map(product => `
     <div class="col">
       <img src="${product.images[0].src}" alt="${product.title}" />
