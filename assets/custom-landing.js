3110e429c321fed4b025b177134840a9a1eb0d2f
Noha Ahmed 01 <nohaniho88@gmail.com>
2025-05-26 21:34:14 +0300

    wed

--- assets/custom-landing.js
+++ assets/custom-landing.js
@@ -33,7 +33,7 @@ document.addEventListener("DOMContentLoaded", async () => {
       const product = data.products[index];
       tcard.innerHTML = `
         <div class="close">
-      <i class="fa-solid fa-xmark"></i>
+      <img src="{{ 'icon-close.svg' | asset_url }}" alt="close">
     </div>
     <div class="top-card">
       <img src="${product.images[0].src}" alt="${product.title}" />
