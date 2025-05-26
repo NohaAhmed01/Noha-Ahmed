49691a7c3decf76fe6f336e54bcb0bd4c784c3aa
Noha Ahmed 01 <nohaniho88@gmail.com>
2025-05-26 22:28:10 +0300

    hhjh

--- assets/custom-landing.js
+++ assets/custom-landing.js
@@ -93,6 +93,7 @@ document.addEventListener("DOMContentLoaded", async () => {
   });
 
   if (tcard) {
+
     tcard.addEventListener("click", (event) => {
       sizesControl();
 
@@ -105,6 +106,19 @@ document.addEventListener("DOMContentLoaded", async () => {
 
       // Add 'active' to the clicked one
       clickedBtn.classList.add("active");
+    });
+         //get the picked color
+    document.querySelectorAll(".cbtn").forEach((btn) => {
+      btn.addEventListener("click", (e) => {
+        selectedColor = e.target.innerText.trim();
+      });
+    });
+    //get the picked size
+    dropdown.querySelectorAll(".dropdown-options li").forEach((option) => {
+      option.addEventListener("click", (e) => {
+        selectedSize = e.target.innerText.trim();
+        placeholder.innerText = selectedSize;
+      });
     });
   }
 
@@ -142,19 +156,7 @@ document.addEventListener("DOMContentLoaded", async () => {
   }
 
   function addToCart() {
-     //get the picked color
-    document.querySelectorAll(".cbtn").forEach((btn) => {
-      btn.addEventListener("click", (e) => {
-        selectedColor = e.target.innerText.trim();
-      });
-    });
-    //get the picked size
-    dropdown.querySelectorAll(".dropdown-options li").forEach((option) => {
-      option.addEventListener("click", (e) => {
-        selectedSize = e.target.innerText.trim();
-        placeholder.innerText = selectedSize;
-      });
-    });
+
     console.log("color is "+selectedColor+" and size is "+selectedSize);
     
     if (!selectedColor || !selectedSize) {
